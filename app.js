23c33f0fd7b511c16d11602c63359aa8166f2f59
alpha74 <amanalphakumar@gmail.com>
2021-02-13 00:18:08 +0530

    Add func to add item.

---
+++ app.js
@@ -0,0 +1,40 @@
+const buttonEl = document.querySelector('button')
+const inputGoalEl = document.getElementById('goal')
+const inputDescEl = document.getElementById('goalDesc')
+const listEl = document.querySelector('ul')
+
+// Function to add an Item
+function addGoal()
+{
+    const enteredGoal = inputGoalEl.value ;
+    const enteredGoalDesc = inputDescEl.value ;
+
+    if( enteredGoal.length > 0 )
+    {
+        const enteredGoalStrong = document.createElement('strong')
+        enteredGoalStrong.textContent = enteredGoal
+
+        var attribId = "goalItem"
+        attribId = attribId.concat( listEl.childElementCount+1 ) 
+
+        const lineBreak = document.createElement('br')
+        const listItem = document.createElement('li');
+        listItem.setAttribute('id', attribId)
+
+        listItem.appendChild(enteredGoalStrong)
+        listItem.append(lineBreak)
+        listItem.append(enteredGoalDesc)
+
+        listEl.appendChild(listItem)
+    }
+    else
+    {
+        alert("Title cannot be empty")
+    }
+
+    // Reset values in form
+    inputGoalEl.value = ''
+    inputDescEl.value = ''
+}
+
+buttonEl.addEventListener('click', addGoal);
