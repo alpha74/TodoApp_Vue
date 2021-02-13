5351090b90de0395cd52a79693fdca747389bbdb
alpha74 <amanalphakumar@gmail.com>
2021-02-13 19:27:57 +0530

    Add Vue app code.

--- app.js
+++ app.js
@@ -1,54 +1,26 @@
-const buttonEl = document.querySelector('button')
-const inputGoalEl = document.getElementById('goal')
-const inputDescEl = document.getElementById('goalDesc')
-const listEl = document.querySelector('ul')
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredTitle: '',
+            enteredDesc: ''
+        } ;
+    },
 
-// Function to remove an Item
-function removeItem(elementId)
-{
-    document.getElementById(elementId).remove()
-}
+    methods: {
+        addGoal() {
+            itemId = this.goals.length
 
-// Function to add an Item
-function addGoal()
-{
-    const enteredGoal = inputGoalEl.value ;
-    const enteredGoalDesc = inputDescEl.value ;
+            this.goals.push( { 'id': itemId, 
+                'title': this.enteredTitle, 
+                'desc': this.enteredDesc
+            } ) ;
+            this.enteredTitle = '' ;
+            this.enteredDesc = '' ;
+        },
 
-    if( enteredGoal.length > 0 )
-    {
-        const enteredGoalStrong = document.createElement('strong')
-        enteredGoalStrong.textContent = enteredGoal
-
-        var attribId = "goalItem"
-        attribId = attribId.concat( listEl.childElementCount+1 ) 
-
-        const buttonDeleteItem = document.createElement('input')
-        buttonDeleteItem.setAttribute('onclick', 'removeItem("' + attribId + '")')
-        buttonDeleteItem.setAttribute('type', 'button')
-        buttonDeleteItem.setAttribute('style', 'margin-top: 8px')
-        
-        buttonDeleteItem.value = "Delete"
-
-        const lineBreak = document.createElement('br')
-        const listItem = document.createElement('li');
-        listItem.setAttribute('id', attribId)
-
-        listItem.appendChild(enteredGoalStrong)
-        listItem.append(lineBreak)
-        listItem.append(enteredGoalDesc)
-        listItem.append(buttonDeleteItem)
-        
-        listEl.appendChild(listItem)
+        removeGoal(goal) {
+            this.goals.splice(this.goals.indexOf(goal), 1) ;
+        }
     }
-    else
-    {
-        alert("Title cannot be empty")
-    }
-
-    // Reset values in form
-    inputGoalEl.value = ''
-    inputDescEl.value = ''
-}
-
-buttonEl.addEventListener('click', addGoal);
+}).mount("#app") ;
